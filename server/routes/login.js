4cd1ea1bb43014a04b3e2653213c0cfc7158e312
kuailedaima <shuaihaome@163.com>
2021-03-19 18:15:43 +0800

    日常

--- server/routes/login.js
+++ server/routes/login.js
@@ -2,23 +2,52 @@ var express = require('express');
 var router = express.Router();
 var mysql = require('mysql')
 var db = require('../db/db')
+const jwt =require('jsonwebtoken')
 
 router.post('/',(req,res) =>{
-    console.log(req.body.useraccount);
-    console.log(req.body.userpassword);
-    var useraccount = req.body.useraccount;
-    var userpassword = req.body.userpassword;
+    console.log(req.body.username);
+    console.log(req.body.password);
+    var useraccount = req.body.username;
+    var userpassword = req.body.password;
     var sql = 'SELECT * FROM users where account = ' +"'" +useraccount+"'"+'and password= ' +"'" +userpassword+"'" ;
     // var sql = "select * from users where account = '张三' ";
     db.query(sql,[],(results) =>{
-        if(results.length ==0) {
-            res.send("0"); //返回用户名或密码错误
+        if(results.length ==0) { //返回用户名或密码错误
+            res.send({
+                data:{
+                    username:useraccount,
+                },
+                meta:{
+                    msg:"用户名或密码错误",
+                    status:202
+                }
+            });      
         } 
-        else if(results.length !=0){
-            res.send("1");      //返回用户名和密码均正确
+        else if(results.length !=0){ //返回用户名和密码均正确
+            //生成token
+            const token = jwt.sign({useraccount},'blog');
+            console.log(token);
+            res.send({
+                data:{
+                    username:useraccount,
+                    token:token
+                },
+                meta:{
+                    msg:"登录成功",
+                    status:200
+                }
+            });      
         }
-        else {
-            res.send("2")       //出错了请稍后
+        else {          //出错了请稍后
+            res.send({
+                data:{
+                    username:useraccount,
+                },
+                meta:{
+                    msg:"出错了请稍后",
+                    status:404
+                }
+            });   
         }
     })
 });
